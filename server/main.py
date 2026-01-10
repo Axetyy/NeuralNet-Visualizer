60c92cab83c793851b78696d194a225aad824e85
Axetyy <iuliannistor06@gmail.com>
2026-01-10 16:31:18 +0200

    dynamic port

--- server/main.py
+++ server/main.py
@@ -198,5 +198,5 @@ async def draw_model(payload: List[float]):
         "predicted": predicted,
     }
 if __name__ == '__main__':
-    import uvicorn 
-    uvicorn.run(app,host='0.0.0.0',port=8000)
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
